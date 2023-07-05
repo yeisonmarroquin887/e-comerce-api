d1ab9093799f3986e5991d80947016d4d25f704e
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-07-05 09:16:09 -0500

    Entregable-Final

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const routerCategory = require('./category.router');
 const routerProduct = require('./product.router');
 const routerCart = require('./cart.router');
 const routerPurchase = require('./purchase.router');
-const routerProductimg = require('./productimg.router');
+const routerProductimg = require('./productImg.router');
 const router = express.Router();
 
 // colocar las rutas aquí
